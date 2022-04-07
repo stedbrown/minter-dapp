877cfa76432fe3994f4b146a9b6b6282dea0e3f1
stedbrown <35691832+stedbrown@users.noreply.github.com>
2022-04-07 13:52:23 +0200

    time

--- backend/src/config.js
+++ backend/src/config.js
@@ -59,7 +59,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-04-06T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-04-06T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 2000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xBd3b7ddE3023565b4222FDcBC0053b691Ab5E0eF"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
