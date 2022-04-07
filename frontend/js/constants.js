2c75746753e6010cdd123b9f619d6beda22feb17
stedbrown <35691832+stedbrown@users.noreply.github.com>
2022-04-07 14:17:31 +0200

    d

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,5 +1,5 @@
 const contractAddress = "0x35Cfe1e60984943Cb7024ECe79686c7E183b481d"; //Replace with your own contract address
-const chain = 'polygon'; // rinkeby or polygon
+const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Handsdrawing FORM NFT drop";
 const welcome_h2 = "Connect to MetaMask to Get Started";
